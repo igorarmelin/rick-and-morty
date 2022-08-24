71ca04ab2ea44cd8a1522f3b2838ff8263025260
igorarmelin <igor_armelin@hotmail.com>
2022-08-24 00:40:14 -0300

    create apollo/graphql config

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,15 @@
+import { ApolloProvider } from "@apollo/client"
 import React from "react"
 import ReactDOM from "react-dom/client"
 import App from "./App"
-import "./index.css"
+import GlobalStyle from "./common/styles/globalStyle"
+import client from "./resources/api/graphql"
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <ApolloProvider client={client}>
+      <GlobalStyle />
+      <App />
+    </ApolloProvider>
   </React.StrictMode>
 )
